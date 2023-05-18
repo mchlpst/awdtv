223270faa0ee6e54defe12a1ba6612368f06f116
Michael Post <michael@egotribe.nl>
2023-05-18 08:01:27 +0200

    removed strapi

--- strapi/src/api/top-menu/controllers/top-menu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * top-menu controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::top-menu.top-menu');
--- strapi/src/api/top-menu/services/top-menu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * top-menu service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::top-menu.top-menu');
