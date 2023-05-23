7fb366435e4301267676b7fe02af29652c84b861
Michael Post <michael@egotribe.nl>
2023-05-23 09:58:36 +0200

    added sportLink fetch

--- client/src/hooks/ClubData.js
+++ client/src/hooks/ClubData.js
@@ -1,10 +1,10 @@
 import React, { useEffect, createContext, useState } from "react";
-import axios from "axios";
 
 export const ClubDataContext = createContext();
 
 export const ClubProvider = ({ children }) => {
   const [clubData, setClubData] = useState(null);
+
   let today = new Date();
 
   let fromDd = String(today.getDate()).padStart(2, "0");
@@ -19,6 +19,14 @@ export const ClubProvider = ({ children }) => {
 
   let fromDate = `${fromYyyy}-${fromMm}-${fromDd}`;
   let toDate = `${toYyyy}-${toMm}-${toDd}`;
+
+  let past = new Date(today.setMonth(today.getMonth() - 10));
+
+  let pastDd = String(past.getDate()).padStart(2, "0");
+  let pastMm = String(past.getMonth() + 1).padStart(2, "0");
+  let pastYyyy = past.getFullYear();
+
+  let pastDate = `${pastYyyy}-${pastMm}-${pastDd}`;
 
   useEffect(() => {
     fetch(
@@ -39,8 +47,68 @@ export const ClubProvider = ({ children }) => {
         }
       })
       .then((res) => {
-        console.log(res);
-        setClubData(res.data);
+        setClubData((prevState) => ({
+          ...prevState,
+          program: {
+            res,
+          },
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    fetch(
+      `https://api-mijn.korfbal.nl/api/v2/clubs/NCX14G7/results?&dateFrom=${pastDate}&dateTo=${fromDate}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw `error with status ${res.status}`;
+        }
+      })
+      .then((res) => {
+        setClubData((prevState) => ({
+          ...prevState,
+          results: {
+            res,
+          },
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    fetch(
+      `https://api-mijn.korfbal.nl/api/v2/pools/club/NCX14G7?sport=KORFBALL-VE-WK&team=true`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw `error with status ${res.status}`;
+        }
+      })
+      .then((res) => {
+        setClubData((prevState) => ({
+          ...prevState,
+          standings: {
+            res,
+          },
+        }));
       })
       .catch((error) => {
         console.log(error);
