8d2c2a7cb32037167dc6bc9d73bca9e307cfca4e
Michael Post <michael@egotribe.nl>
2023-06-01 08:11:05 +0200

    added slug data setup

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import "./App.scss";
 import MainNavigation from "./components/MainNavigation/MainNavigation";
 import TopMenu from "./components/TopMenu/Topmenu";
 import HomePage from "./pages/HomePage/HomePage";
+import SlugPage from "./pages/SlugPage/SlugPage";
 
 const App = () => {
   return (
@@ -16,7 +17,8 @@ const App = () => {
         <MainNavigation />
       </header>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/:slug" element={<SlugPage />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/SlugPage/SlugPage.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { DatoContext } from "../../hooks/datoCMS";
+
+import Grid from "../../layout/Grid/Grid";
+import ComponentLoader from "../../components/ComponentLoader/ComponentLoader";
+
+const SlugPage = () => {
+  const { slug } = useParams();
+
+  const [data, setData] = useState(null);
+  const context = useContext(DatoContext);
+
+  useEffect(() => {
+    if (context) {
+      if (context.allArticles) {
+        let pageItem = context.allArticles.find((item) => {
+          return item.slug === slug;
+        });
+        setData({ pageItem });
+      }
+    }
+  }, [context]);
+
+  console.log();
+  return (
+    <main className="slug-page">
+      {/* <Grid>{data && <ComponentLoader data={data} />}</Grid> */}
+    </main>
+  );
+};
+export default SlugPage;
