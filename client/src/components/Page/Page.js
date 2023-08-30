0cf3930fef6e8d1e32758c94cbcee2d95dfb85df
Michael Post <michael@egotribe.nl>
2023-08-30 21:49:48 +0200

    adds mobile support pdf

--- client/src/components/Page/Page.js
+++ client/src/components/Page/Page.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
+
 import { WhatsappShareButton } from "react-share";
 import { SocialIcon } from "react-social-icons";
+
+import { useViewport } from "../../hooks/useViewport";
 
 import "./Page.scss";
 
 const Page = (props) => {
   const content = props.data;
   const shareUrl = window.location.origin + "/" + content.slug;
-  console.log(content.attachments);
+
+  const { isMobile, isTablet } = useViewport({
+    mobile: 480,
+    tablet: 768,
+    laptop: 1024,
+    desktop: 1200,
+  });
+
   return (
     <section className="page">
       <div className="page__header">
@@ -30,11 +40,28 @@ const Page = (props) => {
         <>
           {content.attachments[0].filename.includes(".pdf") && (
             <section className="page__content">
-              <embed
-                src={content.attachments[0].url}
-                width="100%"
-                height="800px"
-              />
+              {!isTablet && !isMobile ? (
+                <object
+                  data={content.attachments[0].url}
+                  type="application/pdf">
+                  <p>
+                    Het lijkt erop dat je browser dit document niet kan laden.
+                    Je kan het hier downloaden.&nbsp;
+                    <a
+                      href={content.attachments[0].url}
+                      alt="download link"
+                      target="_blank">
+                      Download PDF.
+                    </a>
+                  </p>
+                </object>
+              ) : (
+                <p>
+                  Het lijkt erop dat je browser dit document niet kan laden. Je
+                  kan het hier downloaden.&nbsp;
+                  <a href={content.attachments[0].url}>Download PDF.</a>
+                </p>
+              )}
             </section>
           )}
           {content.attachments[0].filename.includes(".png") && (
