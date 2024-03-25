07eb22d02dbdfa50a4cf66d76865039c74657071
Michael Post <michael@egotribe.nl>
2024-03-25 21:45:38 +0100

    adds production database

---
+++ config/env/production/database.js
@@ -0,0 +1,21 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false) },
+      },
+      ssl: env("DATABASE_SSL", false),
+      debug: false,
+    },
+  };
+};
