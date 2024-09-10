1b19e5956bd7fd8ce461422e9e254e822fc374ff
Michael Post <michael@egotribe.nl>
2024-09-10 21:03:21 +0200

    adds webhook callback

--- server.js
+++ server.js
@@ -16,6 +16,40 @@ app.use("/new-member", require("./routes/newMember"));
 app.use("/custom-form", require("./routes/sendCustomForm"));
 app.use("/send-contact-form", require("./routes/sendContactForm"));
 
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.WEBHOOK_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  const body = req.body;
+
+  if (body.object === "page") {
+    body.entry.forEach((entry) => {
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+
+      // Do your magic here!
+      // Maybe send a witty response?
+      // Update your database?
+      // The world is your oyster!
+    });
+
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
