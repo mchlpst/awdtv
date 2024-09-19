0081411c7c0dd72661e6e4bb0f366844c4c58fb5
Michael Post <michael@egotribe.nl>
2024-09-19 10:22:55 +0200

    removes cors

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/client/build"));
