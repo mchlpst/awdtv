b9ec61134a8814753a9240c184e322409d9525a8
Michael Post <hallo@michaelpost.nl>
2026-01-05 20:26:34 +0100

    fix: update email subject and response message for schoolkorfbal registration

--- routes/api/routes/sendCustomForm.js
+++ routes/api/routes/sendCustomForm.js
@@ -16,9 +16,7 @@ const transporter = nodemailer.createTransport({
 });
 
 transporter.verify((err, succes) => {
-  err
-    ? console.log("validatie mislukt: " + err)
-    : console.log(`Send custom form: ${succes}`);
+  err ? console.log("validatie mislukt: " + err) : console.log(`Send custom form: ${succes}`);
 });
 
 sendEmailRouter.post("/", async (req, res, next) => {
@@ -26,7 +24,7 @@ sendEmailRouter.post("/", async (req, res, next) => {
     from: "Website AWDTV <noreply@awdtv.nl>",
     to: "schoolkorfbal@awdtv.nl",
     // to: "michael@weareonetribe.nl",
-    subject: "Aanmelding schoolkorfbal 2024",
+    subject: "Aanmelding schoolkorfbal 2026",
     html: `
   	<h1>Aanmelding Schoolkorfbal</h1>
   	<p>Hallo Jet,<br/><br/>Er is een nieuwe aanmelding voor schoolkorbal hieronder zijn de gegevens.</p>
@@ -74,7 +72,8 @@ sendEmailRouter.post("/", async (req, res, next) => {
     subject: "Je bericht is goed aangekomen.",
     html: `
 		<h1>Geluk!</h1>
-		<p>We hebben je bericht in goede orde ontvangen. Dit is wat je ons gestuurd hebt:</p>
+		<p>We hebben je bericht in goede orde ontvangen. Na 26 januari krijg je per mail het programma toegestuurd.</p>
+    <p>Dit is wat je ons gestuurd hebt:</p>
     <table border="0">
       <tr>
         <td style="padding: 10px 15px; border: 1px solid black;">Geslacht</td>
@@ -110,13 +109,10 @@ sendEmailRouter.post("/", async (req, res, next) => {
       </tr>
     </table>
     <p>Als er iets niet klopt vul het contact formulier dan opnieuw in.</p>
-    <p> Met vriendelijke groet,<br>Website AW.DTV</p>
+    <p> Met vriendelijke groet,<br>AW.DTV</p>
 		`,
   };
-  Promise.all([
-    transporter.sendMail(messageOwner),
-    transporter.sendMail(messageSender),
-  ])
+  Promise.all([transporter.sendMail(messageOwner), transporter.sendMail(messageSender)])
     .then(res.status(200).json({ emailSend: true }))
     .catch((err) => res.status(err.status || 500));
 });
